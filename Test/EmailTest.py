0239a105fa18cbbfddb8c25aefb4a71d18d4b258
Nanayakkara, Elaine <Elaine.Nanayakkara@syscolabs.com>
2022-02-24 13:02:22 +0530

    Add EmailTest

---
+++ Test/EmailTest.py
@@ -0,0 +1,17 @@
+#Import packages
+
+#Define the class
+
+    #Define a method to compare and export excel file
+        #1. Read 2 CSV files from data folder
+        #2. Compare and get the difference
+        #3. Export the diff_df to an excel file
+
+
+    #Implement the test method to send an email
+        #1. Call compare_two_csv_files method
+        #2. Configure sendGrid API key
+        #3. Compose the message
+        #4. Read and attach the output file
+        #5. Send the Email
+        #6. Assert the result
